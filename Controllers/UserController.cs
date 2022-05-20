28f6c85dfb00407485a5914f95a8ca39b5db1bb6
AlbertChangSLW <55155818+AlbertChangSLW@users.noreply.github.com>
2022-05-20 07:09:50 -0600

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MovieShopMVC.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Purchases()
+        {
+            var userId = Convert.ToInt32(this.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Favorites()
+        {
+            var userId = Convert.ToInt32(this.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Reviews()
+        {
+            var userId = Convert.ToInt32(this.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            return View();
+        }
+    }
+}
